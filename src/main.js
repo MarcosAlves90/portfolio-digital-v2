8c9548aef2c2b7f768d747810d4a1501d246eee6
MarcosAlves90 <MarcosAlves25@proton.me>
2025-04-07 00:05:43 -0300

    add loading spinner and background styling for improved user experience

--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ import App from './App.vue'
 import i18n from './i18n';
 
 createApp(App).use(i18n).mount('#app')
+
+// Remova o loader após o Vue ser montado
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    if (loader) {
+      loader.style.display = 'none';
+    }
+  });
